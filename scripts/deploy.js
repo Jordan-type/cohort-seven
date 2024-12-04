e6657a04580aa9114ea723a8609112043e0e2f73
Jordan <37406048+Jordan-type@users.noreply.github.com>
2024-12-04 21:26:40 +0300

    Jokes contract complete

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,17 +3,23 @@ const hre = require("hardhat");
 async function main() {
     await hre.run('compile');
 
+    const Jokes = await hre.ethers.getContractFactory("Jokes")
+    const jokes = await Jokes.deploy();
+    await jokes.waitForDeployment();
 
-    const TodoRoles = await hre.ethers.getContractFactory("TodoRoles")
-    const todoRoles = await TodoRoles.deploy();
-    await todoRoles.waitForDeployment();
+    console.log("Jokes Contract Address", await jokes.getAddress())
 
-    console.log("Todo Roles Contract Address", await todoRoles.getAddress())
 
-    const Todo = await hre.ethers.getContractFactory("TodoManger");
-    const todo = await Todo.deploy();
-    await todo.waitForDeployment();
-    console.log("Todo Manager Contract Address", await todo.getAddress())
+    // const TodoRoles = await hre.ethers.getContractFactory("TodoRoles")
+    // const todoRoles = await TodoRoles.deploy();
+    // await todoRoles.waitForDeployment();
+
+    // console.log("Todo Roles Contract Address", await todoRoles.getAddress())
+
+    // const Todo = await hre.ethers.getContractFactory("TodoManger");
+    // const todo = await Todo.deploy();
+    // await todo.waitForDeployment();
+    // console.log("Todo Manager Contract Address", await todo.getAddress())
 }
 
 main().then(() => process.exit(0))
